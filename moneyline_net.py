377486c2ca603516025559c3c5d4f6032596681b
Kevin Sounanthanam <kevin.sounanthanam@gmail.com>
2021-07-16 06:29:35 +0200

    add batchnorm layer

--- moneyline_net.py
+++ moneyline_net.py
@@ -9,8 +9,9 @@ Created on Thu Jul  8 22:16:35 2021
 import pandas as pd
 import numpy as np
 
+import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 import torch.optim as optim
 
 FOLDER_PATH = '/home/dalton/Desktop/betting/'
@@ -20,23 +21,25 @@ class moneyline_net(nn.Module):
     def __init__(self):
         super(moneyline_net, self).__init__()
         
+        self.layer0 = nn.BatchNorm1d(84)
         self.layer1 = nn.Linear(84,42)
-        self.layer2 = nn.Linear(42, 42)
+        #self.layer2 = nn.Linear(42, 42)
         self.layer3 = nn.Linear(42,1)
         
     def forward(self, x):
         
+        x = self.layer0(x)
         x = self.layer1(x)
-        x = F.relu(x)
-        x = self.layer2(x)
-        x = F.relu(x)
+        x = nn.functional.relu(x)
+        #x = self.layer2(x)
+        #x = nn.functional.relu(x)
         x = self.layer3(x)
-        x = F.sigmoid(x)
+        x = torch.sigmoid(x)
         return x
         
     def train(self, X, Y, learning_rate = 0.01, iterations = 10):
         
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.BCELoss()
         optimizer = optim.Adam( self.parameters(), lr = learning_rate) 
         loss = 0
         for i in range(iterations):
@@ -49,11 +52,11 @@ class moneyline_net(nn.Module):
         return loss.item(), score
 
     def score(self, X, Y):
-        output = self.forward(X).numpy()
-        return 1 - np.mean(np.abs(output - Y.numpy()))
+        output = (self.forward(X).detach().numpy() > 0.5)*1.0
+        return np.sum(output == Y.numpy())/Y.shape[0]
     
     def compute_loss(self,X , Y):
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.BCELoss()
         output = self.forward(X)
         loss = criterion(output, Y)
         return loss.item()
@@ -66,9 +69,20 @@ class moneyline_net(nn.Module):
             print('====> Epoch: {} train_loss: {:.4f}, train_score: {:.4f}, test_loss: {:.4f}, test_score: {:.4f}'.format(epoch, train_loss, train_score, test_loss, test_score ))
 
             
-Y = pd.read_csv('{}targets.csv'.format(FOLDER_PATH))
+targets = pd.read_csv('{}targets.csv'.format(FOLDER_PATH)).values
+Y_train = torch.tensor(targets[:1000]).float()
+Y_test = torch.tensor(targets[1000:]).float()
+
+features = pd.read_csv('{}features.csv'.format(FOLDER_PATH)).values
+X_train = torch.tensor(features[:1000]).float()
+X_test = torch.tensor(features[1000:]).float()
         
-        
+net = moneyline_net()
+
+
+net.Train(X_train, Y_train, X_test, Y_test, 0.001, 100, 10)
+
+
         
 
 
